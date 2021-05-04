35b89b261ba0f0b42b36b9492f76effd756ede30
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-05-04 19:41:00 +0200

    Update 3-Arrays.php

--- 3-Arrays.php
+++ 3-Arrays.php
@@ -7,4 +7,5 @@ print_r($arr); #makes the array human readable
 array_push($arr, 34); #adds something to the stack
 $arr[] = 45; #add something to the stack
 array_pop($arr); #removes top element of stack
-
+array_shift($arr); #removes the first element in the array and shifts 
+array_unshift($arr, "Hey, "); #adds something to the beginning of the array
